76d8368a852a7846baf0070726260b1f401fea4f
Parvez-bytes <parvezpatel7236@gmail.com>
2026-04-13 22:20:29 +0530

    feat: add mobile menu toggle with slide-in/out functionality

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,11 @@
-const menu =document.querySelector(".menu") ;
-menu.oncl
+const menuicon =document.querySelector(".menu-icon") ;
+const menu =document.querySelector(".menu");
+const menucross=document.querySelector(".menu-cross");
+menuicon.addEventListener("click", ()=>{
+      menu.classList.toggle("active");
+
+}) ;
+
+menucross.addEventListener("click", ()=>{
+    menu.classList.remove("active");
+})
